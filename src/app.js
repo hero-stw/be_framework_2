adac45d77e448adca6db9d1ce9c52dbe5ecf4449
Hero <16040582@vnu.edu.vn>
2022-07-29 08:51:56 +0700

    FA-34 Modify base and add record routes

--- src/app.js
+++ src/app.js
@@ -2,23 +2,19 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
-import BookRoute from "./routes/books";
 import UserRoute from "./routes/users";
-import CategoryRoute from "./routes/category";
-import OrderRoute from "./routes/orders";
+import RecordRoute from "./routes/records";
 
 const app = express();
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 
-app.use("/api", BookRoute);
 app.use("/api", UserRoute);
-app.use("/api", CategoryRoute);
-app.use("/api", OrderRoute);
+app.use("/api", RecordRoute);
 
 mongoose
-  .connect("mongodb://localhost:27017/bookstore")
+  .connect("mongodb://localhost:27017/mathtool")
   .then(() => {
     console.log("Connected to database");
   })
---
+++ src/routes/records.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/records", async (req, res) => {});
+
+router.post("/records", async (req, res) => {});
+
+router.get("/record/:id", async (req, res) => {});
+
+router.put("/record/:id", async (req, res) => {});
+
+router.delete("/record/:id", async (req, res) => {});
+
+export default router;
